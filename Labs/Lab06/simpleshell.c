7f8cb7bc75751bfbac5802b5c3dec5d7e62ea3bf
alexandervansteel <alexandervansteel@gmail.com>
2017-03-14 19:45:55 +0000

    lab06

---
+++ Labs/Lab06/simpleshell.c
@@ -0,0 +1,69 @@
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <string.h>
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <sys/resource.h>
+ #include <sys/time.h>
+ #include <sys/resource.h>
+
+ #define MAXBUF 80
+ #define MAXARG 20
+
+ int main() {
+   char buf[MAXBUF];
+   char* args[MAXARG];
+   char* token;
+   pid_t pid;
+   int status = 0;
+
+   while (1) {
+     printf("SimpleShell>> ");
+     if (fgets(buf, MAXBUF, stdin) == NULL) {
+       printf("ERROR: Failed to get command.\n");
+     }
+     buf[strcspn(buf, "\n")] = 0;
+     if (strcmp(buf, "exit") == 0) {
+       exit(0);
+     }
+
+     token = strtok(buf, " ");
+     int i = 0;
+     while (token != NULL) {
+       char* arg = malloc(sizeof(*arg) * (strlen(token) + 1));
+       strcpy(arg, token);
+       args[i] = arg;
+       token = strtok(NULL, " ");
+       i++;
+     }
+     args[i] = NULL;
+
+     pid = fork();
+     if (pid < 0) {
+       printf("ERROR: Failed to fork.\n");
+     }
+
+     else if (pid) {
+       struct rusage res;
+       wait3(&status, 0, &res);
+
+       struct timeval uTime = res.ru_utime;
+       time_t uTimeSec = uTime.tv_sec;
+       long int uTimeMicroSec = uTime.tv_usec;
+       printf("User CPU Time Usage: %ld %ldus\n", uTimeSec, uTimeMicroSec);
+
+       long int ics = res.ru_nivcsw;
+       printf("Involuntary Context Switches: %ld\n", ics);
+     }
+
+     else {
+      if (execvp(args[0], args) < 0) {
+        printf("ERROR: Failed to create child.\n");
+      }
+      exit(status);
+     }
+  }
+  return 0;
+}
